d6713dbc4ca7b862a077adbdb258935427506699
Fernando Souza <fernandosouzadev@gmail.com>
2023-08-24 16:43:33 -0300

    Ajuste na configuração

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const cors = require('cors')
 const consign = require('consign')
+const config = require('./config/config')
 
 const app = express()
 
 app.use(express.json())
-app.use(cors({ credentials: true, origin: 'http://localhost:4200' }))
+app.use(cors({ credentials: true, origin: config.app.base_url_frontend }))
 app.use(express.static('public'))
 
 consign().include('src/routes').into(app)
